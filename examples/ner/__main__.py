c45484ce40ff7b0ea4b749b0a19aa8f77dcf9d04
Spyros Briakos <spympr.6@gmail.com>
2021-11-23 23:37:56 +0200

    minor

--- examples/ner/__main__.py
+++ examples/ner/__main__.py
@@ -67,7 +67,8 @@ def ner():
 def greek_bert():
     pass
 
-train_path,dev_path,test_path = './examples/ner/NER_Dataset/new_train.txt','./examples/ner/NER_Dataset/new_dev.txt','./examples/ner/NER_Dataset/new_test.txt'
+# train_path,dev_path,test_path = './examples/ner/NER_Dataset/new_train.txt','./examples/ner/NER_Dataset/new_dev.txt','./examples/ner/NER_Dataset/new_test.txt'
+train_path,dev_path,test_path = './examples/ner/NER_Dataset/train.txt','./examples/ner/NER_Dataset/dev.txt','./examples/ner/NER_Dataset/test.txt'
 MODELS = ['alexaapo/greek_legal_bert_v2','nlpaueb/bert-base-greek-uncased-v1','alexaapo/greek_legal_bert_v1']
 
 @greek_bert.command()   
