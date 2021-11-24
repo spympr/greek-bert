878e9cb2c3d997da4ed258385004495c6996a36a
Spyros Briakos <spympr.6@gmail.com>
2021-11-24 12:19:29 +0200

    minor

--- examples/__main__.py
+++ examples/__main__.py
@@ -9,10 +9,7 @@ from .ner.__main__ import ner
 def cli():
     pass
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 3c5e219d50e56caa8be1bdcf847622feb9fcb548
 cli.add_command(xnli)
 # cli.add_command(ud)
 cli.add_command(ner)
