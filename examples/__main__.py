86dc2d97672bf15c8abc320ef14cc1075f627f09
alexaapo <alexaaposto@gmail.com>
2021-11-24 11:59:20 +0200

    edit xnli

--- examples/__main__.py
+++ examples/__main__.py
@@ -4,11 +4,13 @@ from .xnli.__main__ import xnli
 # from .ud.__main__ import ud
 from .ner.__main__ import ner
 
+
 @click.group()
 def cli():
     pass
 
-# cli.add_command(xnli)
+
+cli.add_command(xnli)
 # cli.add_command(ud)
 cli.add_command(ner)
 
