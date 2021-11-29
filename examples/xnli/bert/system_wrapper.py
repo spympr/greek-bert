72e1690e002ab72e19a937881c8dcef6684b5c1c
alexaapo <alexaaposto@gmail.com>
2021-11-29 13:44:42 +0200

    classification_report

--- examples/xnli/bert/system_wrapper.py
+++ examples/xnli/bert/system_wrapper.py
@@ -154,7 +154,7 @@ class XNLIBERTSystemWrapper:
         print(len(predictions['outputs'][0]))
         print((predictions['outputs'][0]))
         # print(predictions)
-        for i in predictions:
+        for i in predictions['outputs']:
             print(len(i))
 
         target_names = ['neutral', 'contradiction', 'entailment']
