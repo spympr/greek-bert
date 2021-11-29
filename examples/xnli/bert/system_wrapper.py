892629488bc322b317e8e98449b578d8ed0530ec
alexaapo <alexaaposto@gmail.com>
2021-11-29 13:02:31 +0200

    classification_report

--- examples/xnli/bert/system_wrapper.py
+++ examples/xnli/bert/system_wrapper.py
@@ -139,7 +139,7 @@ class XNLIBERTSystemWrapper:
         print(eval_dataloader)
         print(type(eval_dataloader))
         for batch_idx, samples in enumerate(eval_dataloader):
-            print(batch_idx, samples['Class'])
+            print(batch_idx, samples['target'])
         predictions = self._system.predict(eval_dataloader)
         # print(type(predictions))
         # print(predictions.keys())
